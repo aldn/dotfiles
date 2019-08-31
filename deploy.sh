2c5e50095c7742fa1cc83b1b380ab4a440ab8ebe
Oleksandr Dunaievskyi <od@xyzw.me>
2019-08-31 16:25:34 +0300

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+cd
+mkdir .config
+
+# in alphabetical order
+ln -s .cfg/bash/.bashrc
+ln -s .cfg/git/.gitconfig
+ln -s .cfg/mpv .config/mpv
+ln -s .cfg/nvim .config/nvim
+ln -s .cfg/ranger .config/ranger
+ln -s .cfg/rofi .config/rofi
+ln -s .cfg/sway .config/sway
+ln -s .cfg/tmux/.tmux.conf
+ln -s .cfg/vim .vim
+ln -s .cfg/zsh/.zshrc
+
