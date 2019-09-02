f1108000be426288bbd29d67476befdd84dfb710
Oleksandr Dunaievskyi <od@xyzw.me>
2019-09-02 04:22:54 +0300

    removed leading dots from stored config file names as .file -> file

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ cd
 mkdir .config
 
 # in alphabetical order
-ln -s .cfg/bash/.bashrc
-ln -s .cfg/git/.gitconfig
+ln -s .cfg/bash/bashrc .bashrc
+ln -s .cfg/git/gitconfig .gitconfig
 ln -s ../.cfg/mpv .config/mpv
 ln -s ../.cfg/nvim .config/nvim
 ln -s ../.cfg/ranger .config/ranger
 ln -s ../.cfg/rofi .config/rofi
 ln -s ../.cfg/sway .config/sway
 ln -s ../.cfg/termite .config/termite
-ln -s .cfg/tmux/.tmux.conf
+ln -s .cfg/tmux/tmux.conf .tmux.conf
 ln -s .cfg/vim .vim
-ln -s .cfg/zsh/.zshrc
+ln -s .cfg/zsh/zshrc .zshrc
 
