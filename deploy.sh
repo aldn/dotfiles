13b746cbbeef9f2bd23d6df9b4add5d09c55bb9b
Oleksandr Dunaievskyi <od@xyzw.me>
2019-09-03 01:32:44 +0300

    fixed deployment and added platform specific configs for macos

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
+
+case "$(uname)" in
+    Darwin)
+        os="macos"
+        ;;
+    Linux)
+        os="linux"
+        ;;
+    *)
+        echo "error: platform not supported"
+        ;;
+esac
+
 cd
 mkdir .config
 
 # in alphabetical order
-ln -s .cfg/bash/bashrc .bashrc
+
+[ "$os" != "macos" ] && ln -s .cfg/bash/bashrc .bashrc
 ln -s .cfg/git/gitconfig .gitconfig
-ln -s ../.cfg/mpv .config/mpv
-ln -s ../.cfg/nvim .config/nvim
-ln -s ../.cfg/ranger .config/ranger
-ln -s ../.cfg/rofi .config/rofi
-ln -s ../.cfg/sway .config/sway
-ln -s ../.cfg/termite .config/termite
+
+mkdir -p .config/mpv
+if [ "$os" == "macos" ]; then
+    ln -s ../../.cfg/mpv/mpv.conf.macos .config/mpv/mpv.conf
+else
+    ln -s ../../.cfg/mpv/mpv.conf .config/mpv/mpv.conf
+fi
+
+ln -s ../.cfg/nvim .config
+ln -s ../.cfg/ranger .config
+ln -s ../.cfg/rofi .config
+ln -s ../.cfg/sway .config
+ln -s ../.cfg/termite .config
 ln -s .cfg/tmux/tmux.conf .tmux.conf
-ln -s .cfg/vim .vim
+if [ ! -d .vim ]; then
+    ln -s .cfg/vim .vim
+else
+    echo "ln: .vim: directory exists"
+fi
 ln -s .cfg/zsh/zshrc .zshrc
 
